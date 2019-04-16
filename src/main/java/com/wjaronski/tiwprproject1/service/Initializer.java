5fd4be4eadc998360409c5213780483955f9e4a5
Wojciech Jaronski <wojciech.jaronski@impaqgroup.com>
2019-04-16 17:08:35 +0200

    Fix duplicate key in initializer

--- src/main/java/com/wjaronski/tiwprproject1/service/Initializer.java
+++ src/main/java/com/wjaronski/tiwprproject1/service/Initializer.java
@@ -1,7 +1,6 @@
 package com.wjaronski.tiwprproject1.service;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 import com.wjaronski.tiwprproject1.model.Meal;
 import com.wjaronski.tiwprproject1.model.Order;
 import com.wjaronski.tiwprproject1.repository.MealRepository;
@@ -24,7 +23,7 @@ public class Initializer {
                        OrderRepository orders) {
         List<Meal> mealList = (List<Meal>) meals.findAll();
 
-        if(mealList.isEmpty()){
+        if (mealList.isEmpty()) {
             meals.saveAll(ImmutableList.of(
                     new Meal(1, "Burger", "Opis burgera", 122.3, 10.5),
                     new Meal(2, "Tortilla", "Tortilla pszenna", 100.2, 15.99),
@@ -32,13 +31,15 @@ public class Initializer {
             ));
             mealList = (List<Meal>) meals.findAll();
         }
-        
-        log.info("Initializing ORDERS");
-        orders.save(Order.builder()
-                .meals(ImmutableList.of(mealList.get(0), mealList.get(1)))
-                .orderNum(UUID.randomUUID())
-//                .orderNum(UUID.fromString("1"))
-                .build());
+        List<Order> orderList = (List<Order>) orders.findAll();
+        if (orderList.isEmpty()) {
+            log.info("Initializing ORDERS");
+            orders.save(Order.builder()
+                    .meals(ImmutableList.of(mealList.get(0), mealList.get(1)))
+                    .orderNum(UUID.randomUUID())
+                    .build());
+        }
+
     }
 
 }
