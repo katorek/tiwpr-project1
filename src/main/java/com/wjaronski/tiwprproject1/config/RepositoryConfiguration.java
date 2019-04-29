d05fa13be6496620318d13e1b38092d4433ff923
Wojciech Jaroński <wojtasj1996@gmail.com>
2019-04-29 13:09:09 +0200

    Enable CORS

---
+++ src/main/java/com/wjaronski/tiwprproject1/config/RepositoryConfiguration.java
@@ -0,0 +1,15 @@
+package com.wjaronski.tiwprproject1.config;
+
+import com.wjaronski.tiwprproject1.model.Meal;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RepositoryConfiguration extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Meal.class);
+    }
+}
