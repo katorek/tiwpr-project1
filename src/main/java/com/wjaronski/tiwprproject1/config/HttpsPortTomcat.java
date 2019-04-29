636ad511029eabc36eba73d8115838a4376af060
Wojciech Jaroński <wojtasj1996@gmail.com>
2019-04-29 19:35:29 +0200

    Update pom

---
+++ src/main/java/com/wjaronski/tiwprproject1/config/HttpsPortTomcat.java
@@ -0,0 +1,51 @@
+package com.wjaronski.tiwprproject1.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class HttpsPortTomcat {
+
+    @Value("${server.port}")
+    int httpsPort;
+    @Value("${http.port}")
+    int httpPort;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        log.info("Http: {}", httpPort);
+        log.info("Https: {}", httpsPort);
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+        return connector;
+    }
+}
